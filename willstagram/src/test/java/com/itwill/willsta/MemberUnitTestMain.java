d146adc117941fe8a6f31dbd7ddfe5fedfc75c22
kjh <kjh@DESKTOP-GN7ITJR>
2020-03-28 15:46:22 +0900

    membertest

--- willstagram/src/test/java/com/itwill/willsta/MemberUnitTestMain.java
+++ willstagram/src/test/java/com/itwill/willsta/MemberUnitTestMain.java
@@ -1,10 +1,15 @@
 package com.itwill.willsta;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class MemberUnitTestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ApplicationContext applicationContext = new FileSystemXmlApplicationContext("/src/main/webapp/WEB-INF/spring/root-context.xml");
+	
+	
 	}
 
 }
