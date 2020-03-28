a11691871beeaef2937109003ed6144aa71d41d1
jjune1207 <12_sunkist_7@naver.com>
2020-03-28 15:05:24 +0900

    .

--- willstagram/src/test/java/com/itwill/willsta/CommentsUnitTestMain.java
+++ willstagram/src/test/java/com/itwill/willsta/CommentsUnitTestMain.java
@@ -1,9 +1,20 @@
 package com.itwill.willsta;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.itwill.willsta.domain.Comments;
+import com.itwill.willsta.repository.CommentsDaoImpl;
+
 public class CommentsUnitTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/root-context.xml");
+	//	CommentsDaoImpl commentDaoImpl = applicationContext.getBean(CommentsDaoImpl.class);
+		
+		//commentDaoImpl.createComments(new Comments(-999, 22, "hjs", "DAO test"));
+		
 
 	}
 
