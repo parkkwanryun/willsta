dc3340d515cc07ba409680a06a078ce4ff1067c2
wnstn <wnstn@DESKTOP-RCFI363>
2020-03-28 16:58:04 +0900

    aa

--- willstagram/src/test/java/com/itwill/willsta/DMUnitTestMain.java
+++ willstagram/src/test/java/com/itwill/willsta/DMUnitTestMain.java
@@ -3,15 +3,19 @@ package com.itwill.willsta;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.itwill.willsta.repository.DmDao;
 import com.itwill.willsta.repository.DmDaoImpl;
-import com.itwill.willsta.repository.PostDao;
-import com.itwill.willsta.repository.PostDaoImpl;
 
 public class DMUnitTestMain {
 
 	public static void main(String[] args) throws Exception{
 		ApplicationContext applicationContext =
 				new FileSystemXmlApplicationContext("/src/main/webapp/WEB-INF/spring/root-context.xml");	
-		PostDao dao = (PostDaoImpl)applicationContext.getBean(PostDaoImpl.class);
+		/*
+		DMMapper mapper = (DMMapper)applicationContext.getBean(DMMapper.class);
+		System.out.println(mapper.dmSelectAll());
+		*/
+		DmDao dao = applicationContext.getBean(DmDaoImpl.class);
+		System.out.println(dao.dmSelectAll());
 	}
 }
