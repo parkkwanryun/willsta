330105ea198f426b1e0e77226c74802bed5540b4
jjune1207 <12_sunkist_7@naver.com>
2020-03-28 23:56:17 +0900

    .

--- willstagram/src/main/java/com/itwill/willsta/repository/CommentsDao.java
+++ willstagram/src/main/java/com/itwill/willsta/repository/CommentsDao.java
@@ -7,26 +7,26 @@ import com.itwill.willsta.domain.Comments;
 public interface CommentsDao {
 
 	//댓글 작성
-	public int createComments(Comments comments) throws Exception;
+	int createComments(Comments comments) throws Exception;
 
 	//댓글 수정
-	public int updateComments(Comments comments) throws Exception;
+	int updateComments(Comments comments) throws Exception;
 
 	//댓글 삭제
-	public int removeComments(int cNo) throws Exception;
+	int removeComments(int cNo) throws Exception;
 
 	//댓글 조회
-	public Comments findComments(int cNo) throws Exception;
+	Comments findComments(int cNo) throws Exception;
 
 	//댓글 전체 조회
-	public List<Comments> findCommentsList() throws Exception;
+	List<Comments> findCommentsList() throws Exception;
 	
 	//하나의 포스트에 따른 한 맴버의 댓글 수
-	public int postCommentsCountBymId(int pNo, String mId) throws Exception;
+	int postCommentsCountBymId(int pNo, String mId) throws Exception;
 	
 	//한 맴버가 작성한 총 댓글 수
-	public int totalCommentsCountBymId(String mid) throws Exception;
+	int totalCommentsCountBymId(String mid) throws Exception;
 	
 	//하나의 포스트에 달린 총 댓글 수
-	public int postCommentsCount(int pNo) throws Exception;
+	int postCommentsCount(int pNo) throws Exception;
 }
