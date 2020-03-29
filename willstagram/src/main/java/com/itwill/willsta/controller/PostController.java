1094ebeca5a0a706939a2700eec15eebda08ae5e
jsHan <jsHan@jsHan8088>
2020-03-29 19:50:51 +0900

    11

--- willstagram/src/main/java/com/itwill/willsta/controller/PostController.java
+++ willstagram/src/main/java/com/itwill/willsta/controller/PostController.java
@@ -1,8 +1,6 @@
 package com.itwill.willsta.controller;
 
-import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
