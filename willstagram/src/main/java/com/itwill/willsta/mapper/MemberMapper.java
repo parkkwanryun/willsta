296b67738d5161c6b9e598e191cf7ee514ce80dd
stu <stu@stu-PC>
2020-03-30 18:06:33 +0900

    ss

--- willstagram/src/main/java/com/itwill/willsta/mapper/MemberMapper.java
+++ willstagram/src/main/java/com/itwill/willsta/mapper/MemberMapper.java
@@ -19,8 +19,8 @@ public interface MemberMapper {
 	public boolean insertMember(Member member);
 	
 	/*회원정보 로딩*/
-	@Select("SELECT mId, mPass, mName, mEmail, mPhone, mImage FROM MEMBER")
-	public Member selectById(@Param("mId") String mId, @Param("mPhone") String mPhone);
+	@Select("SELECT mId, mPass, mName, mEmail, mPhone, mImage FROM MEMBER WHERE mId=#{mId}")
+	public Member selectById(@Param("mId") String mId);
 	
 	/*회원정보 수정*/
 	@Update("UPDATE MEMBER SET mPass=#{mPass}, mName=#{mName}, mEmail=#{mEmail}, mPhone=#{mPhone}, mImage=#{mImage} WHERE mId=#{mId}")
