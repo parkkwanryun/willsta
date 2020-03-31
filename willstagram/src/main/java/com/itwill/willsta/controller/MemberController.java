ab1803485c7c52d486a143721cce786555f4306e
SEC <SEC@SEC-PC>
2020-03-31 17:46:01 +0900

    .

--- willstagram/src/main/java/com/itwill/willsta/controller/MemberController.java
+++ willstagram/src/main/java/com/itwill/willsta/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.itwill.willsta.domain.Member;
@@ -95,5 +97,15 @@ public class MemberController {
 		return mv;
 	}
 	
+	//@MemberLoginCheck
+	@ResponseBody
+	@RequestMapping(value = "/search_member", method = RequestMethod.GET)
+	public ModelAndView findMemberList(@RequestParam(value = "mId") String mId) {
+		ModelAndView mv=new ModelAndView();
+		List<Member> findMemberList=memberService.findMemberList(mId);
+		mv.addObject("findMemberList",findMemberList);
+		mv.setViewName("profiles");
+		return mv;
+	}
 	
 }
