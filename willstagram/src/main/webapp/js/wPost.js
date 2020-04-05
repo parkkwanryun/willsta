b37f5927e7ba848bf59566822f263a570c028026
jsHan <jsHan@jsHan8088>
2020-04-05 09:31:46 +0900

    무한스크롤

--- willstagram/src/main/webapp/js/wPost.js
+++ willstagram/src/main/webapp/js/wPost.js
@@ -121,7 +121,27 @@ function post_write(){
 						$('div.posts-section').append(resultText);
 					}
 				});
-		        
+		        e.preventDefault();
+		    });
+			
+			//스크롤이벤트 : 문서의 끝 위치에 오면 post 추가 조회
+			$(window).on("scroll", function(e){
+				//문서의높이에 윈도우높이를 제외한 값이 스크롤의 최대값이다
+				if(($(document).height()-$(window).height()) != $(document).scrollTop()){
+					return;
+				}				
+		        var $post = $("div.post-bar").last();
+				var params = "lastpNo="+ $post.attr('post_no');
+		        $.ajax({
+					url:'add_post',
+					method:'POST',
+					data:params,
+					dataType:'html',
+					success: function(resultText){
+						$('div.posts-section').append(resultText);
+					}
+				});
+		        e.preventDefault();
 		    });
 			
 			
@@ -197,6 +217,8 @@ function post_write(){
 				});
 				e.preventDefault();
 			});
+			
+			
 			var sel_files=[];
 			$(document).on('change', 'input.TXTFLD', function(e){
 				var files = e.target.files;
