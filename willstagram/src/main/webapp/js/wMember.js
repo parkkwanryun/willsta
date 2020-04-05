fb6016e572f5a8af1e6219a405cdad7a076be2ff
bluep <bluep@DESKTOP-GQPU77K>
2020-04-05 19:45:56 +0900

    profile-account-setting (회원정보 변경 쪽 수정)

--- willstagram/src/main/webapp/js/wMember.js
+++ willstagram/src/main/webapp/js/wMember.js
@@ -1,18 +1,19 @@
 // 로그인 form 처리 
 
 	/*
-	 1. id 와 password value 값 => 로그인 정보를 컨트롤러로 html 형태로 전송 (완료)
+	 1. 로그인
+	  id 와 password value 값 => 로그인 정보를 컨트롤러로 html 형태로 전송 (완료)
 	 
-	 2. id, password 유효성 체크
+	 2. id, password 유효성 체크(validate)
 	  자바 단에서 요청받은 컨트롤러에서의 세션 유무 체크(Interceptor)  => id/ password 틀릴 시 안내문구 띄워주기(jsp)
 	  
 	 3. session 유지 시간 세팅 (web.xml --> session 유효시간 = 1day ms) (완료)
 	 
-	 4. 회원가입
+	 4. 회원가입 (진행중) insert.
 	 
-	 5. 회원탈퇴( mRetire -> T )
+	 5. 회원탈퇴( mRetire -> T ) remove. 
 	 
-	 6. 회원정보 수정 (Account Setting)
+	 6. 회원정보 수정 (Account Setting) update.
 	 */ 
 
 
@@ -52,7 +53,6 @@ $(function() {
 	/*
 	  회원가입 
 	*/
-		
 	$(document).on(
 			'submit',
 			'#member_register_action',
@@ -68,19 +68,28 @@ $(function() {
 					url : 'sign_up_action',
 					data : params,
 					method : 'POST',
-					dataType : 'text',
-					success : function(textData) {
-						if (textData.trim() == "true") {
-							alert(member_register_action.mId.value
-									+ '님 회원가입을 축하드립니다.');
-							location.href = 'willstagram/index';
-						} else if (textData.trim() == "false") {
-							location.href = 'willstagram/sign_in';
+					dataType : 'json',
+					success : function(jsonObject) {
+						
+						member_register_action.mId.value = jsonObject.mId;
+						member_register_action.mPass.value = jsonObject.mPass;
+						member_register_action.mName.value = jsonObject.mName;
+						member_register_action.mEmail.value = jsonObject.mEmail;
+						member_register_action.mPhone.value = jsonObject.mPhone;
+						member_register_action.mImage.value = jsonObject.mImage;
+						member_register_action.mRetire.value = jsonObject.mRetire;
+						
+						if (jsonObject.trim() == "true") {
+							alert(member_login_action.mId.value+'님 환영합니다.');
+							location.href = '/willstagram/index';
+						} else if (jsonObject.trim() == "false") {
+
 						}
-					}
-				});
-				e.preventDefault();
-			})
+						location.href = 'willstagram/index';
+					} 
+			});
+				//e.preventDefault();
+	})
 });
 
 		
