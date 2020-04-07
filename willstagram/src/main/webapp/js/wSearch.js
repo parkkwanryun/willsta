b389106b1e6cd5f90871c5c8d5719f2a452007b4
SEC <SEC@SEC-PC>
2020-04-07 14:16:12 +0900

    .

--- willstagram/src/main/webapp/js/wSearch.js
+++ willstagram/src/main/webapp/js/wSearch.js
@@ -1,8 +1,29 @@
 $(function(){
-	
+	/*
+	//스크롤이벤트 : 문서의 끝 위치에 오면 사용자 추가 조회
+    $(window).on("scroll", function(e){
+       //문서의높이에 윈도우높이를 제외한 값이 스크롤의 최대값이다
+       if(($(document).height()-$(window).height()) != $(document).scrollTop()){
+          return;
+       }            
+       var $member = $("div.company-up-info").last();
+       var params = "lastId="+ $member.attr('mIdYou');
+       console.log(params);
+         $.ajax({
+          url:'profiles',
+          method:'POST',
+          data:params,
+          dataType:'html',
+          success: function(resultText){
+             $('div.companies-list').append(resultText);
+          }
+       });
+         e.preventDefault();
+     });
+	*/
 	//팔로우 여부 체크
 	var mIdArray=$('#mId-List').serializeArray();
-	console.log(mIdArray);
+	//console.log(mIdArray);
 	for (var i = 0; i < mIdArray.length; i++) {
 		var mId=mIdArray[i].value;
 		var param="mIdYou="+mId;
@@ -14,12 +35,12 @@ $(function(){
 			async: false,
 			success:function(resultText){
 				if (resultText.trim()=='true') {
-					console.log(mId);
-					console.log(resultText.trim())
+					//console.log(mId);
+					//console.log(resultText.trim())
 					$("h3:contains("+mId+")").next().next().children().eq(1).children().hide();
 				}else if (resultText.trim()=='false') {
-					console.log(mId);
-					console.log(resultText.trim())
+					//console.log(mId);
+					//console.log(resultText.trim())
 					$("h3:contains("+mId+")").next().next().children().first().children().hide();
 				}
 			}
@@ -27,13 +48,12 @@ $(function(){
 		});
 	}
 	
-	//검색 두번했을때 버튼 다시 보임..
 	//사용자 검색
 	$('.userSearch').submit(function(e) {
 		
 		//키워드 값 변수에 저장
 		userKeyword="findId="+$('input[name=search]').val();
-		console.log(userKeyword);
+		//console.log(userKeyword);
 		
 		$.ajax({
 			url:'search_member',
@@ -47,7 +67,6 @@ $(function(){
 			}
 		});
 		e.preventDefault();
-		
 	});
 	
 	//팔로우
@@ -84,27 +103,8 @@ $(function(){
 		e.preventDefault();
 	});
 	
-	/*
-	//스크롤이벤트 : 문서의 끝 위치에 오면 사용자 추가 조회
-    $(window).on("scroll", function(e){
-       //문서의높이에 윈도우높이를 제외한 값이 스크롤의 최대값이다
-       if(($(document).height()-$(window).height()) != $(document).scrollTop()){
-          return;
-       }            
-       var $member = $("div.company-up-info").last();
-       var params = "lastId="+ $member.attr('mIdYou');
-       console.log(params);
-         $.ajax({
-          url:'profiles',
-          method:'POST',
-          data:params,
-          dataType:'html',
-          success: function(resultText){
-             $('div.companies-list').append(resultText);
-          }
-       });
-         e.preventDefault();
-     });
-     */
+	
+	
+     
 	
 }); 
