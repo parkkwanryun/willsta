fa4e54353fde7d26d2d3e903a5a3a57f0497b052
jjune1207 <12_sunkist_7@naver.com>
2020-04-07 18:46:52 +0900

    댓글 작성 후 post-comment-list 보여지기 성공

--- willstagram/src/main/webapp/js/wComments.js
+++ willstagram/src/main/webapp/js/wComments.js
@@ -1,9 +1,10 @@
 // 포스트-댓글 전체 보이기 ajax요청
 function postCommentsListFunction($postComments){
 	var params = "pNo="+$postComments.attr("post_no");
+	var pNo = $postComments.attr("post_no");
 	console.log(params);
 	if($postComments.children().length > 1){
-		$postComments.children().fadeToggle(300);
+		$postComments.children().fadeToggle(600);
 	}else {
 		$.ajax({
 			url : "postCommentsList",
@@ -16,18 +17,18 @@ function postCommentsListFunction($postComments){
 				$.each(jsonArray, function(i, jsonObject){
 					jsonObject = jsonArray[i];
 					var cNo = jsonObject.cNo;
-					var mId = jsonObject.mId;
+ 					var mId = jsonObject.mId;
 					var cTime = jsonObject.cTime;
 					var cContents = jsonObject.cContents;
-					html += "<div class='comment-sec' style='display:none'>" +
+					html += "<div class='comment-sec' style='display:none' comments_no='"+cNo+"'>" +
 							"<ul>" +
 							"	<li>" +
 							"		<div class='comment-list'>"	+	
-							"			<div class='comment' comments_no='"+cNo+"'>" +
+							"			<div class='comment' >" +
 							"				<h3>"+mId+"</h3>" +
 							"				<span><img src='images/clock.png' alt=''>"+cTime+"</span>" +
 							"				<p>"+cContents+"</p>" +
-							"				<a href='#' title='' class='active'><i class='fa fa-reply-all'></i>Reply</a>" +
+							"				<a href='#' class='active' comments_no='"+cNo+"'><i class='fa fa-reply-all'></i>Reply</a>" +
 							"			</div>" +
 							" 		</div>" +	
 							"	</li>" +
@@ -35,7 +36,7 @@ function postCommentsListFunction($postComments){
 							"</div>";
 				});
 				$postComments.append(html);
-				$postComments.children().fadeToggle(300);
+				$postComments.children().fadeToggle(600);
 			}
 		});
 	}
@@ -43,7 +44,7 @@ function postCommentsListFunction($postComments){
 
 
 //댓글 작성 ajax 요청
-function commentsInsertActionFunction($comments){
+function commentsInsertActionFunction($comments,no){
 	var params = $comments.serialize();
 	console.log(params);
 	$.ajax({
@@ -54,9 +55,12 @@ function commentsInsertActionFunction($comments){
 		success : function(result) {
 			if(result.trim() == "true"){
 				alert("댓글 쓰기 성공");
+				$comments[0].reset();
+				$("a[post_no='"+no+"']").trigger('click');
 				
 			}else if(result.trim() == "false"){
-				
+				alert("댓글 쓰기 실패");
+				$comments.select();
 			}
 		}
 	});
@@ -77,7 +81,14 @@ $(function() {
 	$(document).on("click", ".comments_insert_button", function(e){
 		console.log(e.target);
 		var $comments = $(".post-bar-"+$(e.target).attr("post_no")).find(".comments_insert_form");
-		commentsInsertActionFunction($comments);
+		console.log($comments);
+		commentsInsertActionFunction($comments, $(e.target).attr("post_no"));
 		e.preventDefault();
+	});
+	
+	//대댓글 쓰기
+	$(document).on("click", ".active", function(e){
+		console.log(e.target);
+		console.log($(e.target))
 	});
 });
