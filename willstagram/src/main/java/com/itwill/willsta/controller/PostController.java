94ce600aee40b11bb810074506e9726245274246
stu <stu@stu-PC>
2020-04-14 17:07:52 +0900

    dd

--- willstagram/src/main/java/com/itwill/willsta/controller/PostController.java
+++ willstagram/src/main/java/com/itwill/willsta/controller/PostController.java
@@ -110,6 +110,7 @@ public class PostController {
 
 		return mv;
 	}
+	
 	@ResponseBody
 	@MemberLoginCheck
 	@RequestMapping(value="/delete_post", method = RequestMethod.POST, produces = "text/plain;charset=utf-8")
